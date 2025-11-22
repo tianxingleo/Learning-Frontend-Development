d382d9a012db9f3107d1e954fb1d8c554f401af9
tianxingleo <tianxingleo@gmail.com>
2025-11-22 18:24:46 +0800

    vue 45课

--- Vue/2025.11.18/src/main.ts
+++ Vue/2025.11.18/src/main.ts
@@ -3,8 +3,18 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
+
 //创建应用
 const app = createApp(App)
+
+//引入pinia
+import { createPinia } from 'pinia'
+//第二步，创建pinia
+const pinia = createPinia()
+//安装pinia
+app.use(pinia)
+
+
 
 //挂载
 app.mount('#app')
